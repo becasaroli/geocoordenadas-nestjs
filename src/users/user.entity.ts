e05451d583caa60f9f0ccce7d77750fd81d35a8b
Bernardo Nicolao <bernardonicolao@Air-de-Bernardo.bbrouter>
2022-07-06 18:59:47 -0300

    commit

---
+++ src/users/user.entity.ts
@@ -0,0 +1,37 @@
+import {
+  BaseEntity,
+  Entity,
+  Unique,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+@Unique(['email'])
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ nullable: false, type: 'varchar', length: 30 })
+  email: string;
+
+  @Column({ nullable: false, type: 'varchar', length: 50 })
+  name: string;
+
+  @Column({ nullable: false })
+  password: string;
+
+  @Column({ nullable: true, type: 'varchar', length: 64 })
+  confirmationToken: string;
+
+  @Column({ nullable: true, type: 'varchar', length: 64 })
+  recoverToken: string;
+
+  @CreateDateColumn()
+  createAt: Date;
+
+  @UpdateDateColumn()
+  updateAt: Date;
+}
